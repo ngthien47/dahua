af9867674eb86fc3e6374bf54e20282dc91a75d9
thientn20 <thientn20@uef.edu.vn>
2024-05-28 13:39:02 +0700

    change code

--- main.js
+++ main.js
@@ -37,4 +37,23 @@ ScrollReveal().reveal("#dvr", {
 ScrollReveal().reveal(".row-dvr", {
 	...scrollRevealOption,
 	delay: 2000,
-}); 
+});
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+	let parent = this.parentNode;
+	parent.classList.add("focus");
+}
+
+function blurFunc() {
+	let parent = this.parentNode;
+	if (this.value == "") {
+		parent.classList.remove("focus");
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener("focus", focusFunc);
+	input.addEventListener("blur", blurFunc);
+});
